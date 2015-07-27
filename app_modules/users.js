8c64233b9d9cc3c847e3ec392106786e8177f798
bluechiptony <tony5egwu@gmail.com>
2015-07-27 13:38:07 +0000

    1. module foder for application modules created 2. user route created 3. server run on codio box

---
+++ app_modules/users.js
@@ -0,0 +1,18 @@
+/**
+* Created with YouKneeFayed-API.
+* User: bluechiptony
+* Date: 2015-07-27
+* Time: 01:17 PM
+* To change this template use Tools | Templates.
+*/
+
+
+var request = require('request');
+var restify = require('restify');
+var csprng = require('csprng');
+var sha1 = require('sha1');
+
+exports.getUsers = function(req, res){
+    res.send("Blahahahhahah");
+    res.end();    
+};
--- youkneefayed_server.js
+++ youkneefayed_server.js
@@ -6,4 +6,35 @@
 * To change this template use Tools | Templates.
 */
 
+//assign required modules
+var request = require('request');
+var restify = require('restify');
+var csprng = require('csprng');
+var sha1 = require('sha1');
 
+
+
+//custom modules for application objects
+var users = require('./app_modules/users.js');
+
+
+
+var server = restify.createServer();
+server.use(restify.bodyParser());
+server.use(restify.authorizationParser());
+server.use(restify.queryParser());
+
+
+//listen on port. 8080 for start can be changed
+server.listen(8080, function(){
+    //console message
+    console.log('Server On');
+    
+    //get routes
+    server.get('/', function(){
+       console.log('In basic endpoint'); 
+    });
+    
+    server.get('/users', users.getUsers);
+    
+});
